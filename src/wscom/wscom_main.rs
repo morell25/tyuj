850e893c1f3dcf7d0f9a1e778351ad3a28c5a20e
morell25 <pablomorellvidal@gmail.com>
2026-02-13 19:31:55 +0100

    Refactor main.rs and add t1.rs for process management functionality

--- src/wscom/wscom_main.rs
+++ src/wscom/wscom_main.rs
@@ -1,22 +1,28 @@
 use core::time;
-use std::{collections::{HashMap, VecDeque}, fmt::format, sync::{Arc, Mutex}, thread, time::Duration};
+use std::{
+    collections::{HashMap, VecDeque},
+    fmt::format,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
-pub fn wscom(){
+pub fn wscom() {
     let cache: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
-    let tareas:Arc<Mutex<VecDeque<String>>> = Arc::new(Mutex::new(VecDeque::new()));
+    let tareas: Arc<Mutex<VecDeque<String>>> = Arc::new(Mutex::new(VecDeque::new()));
     let mut handles = vec![];
     let urls = vec![
-    "https://google.com",
-    "https://rust-lang.org",
-    "https://github.com",
-    "https://google.com",      // Repetida: Debería leer de caché
-    "https://stackoverflow.com",
-    "https://rust-lang.org",   // Repetida: Debería leer de caché
-    "https://wikipedia.org",
-    "https://reddit.com",
-    "https://github.com",      // Repetida: Debería leer de caché
-    "https://amazon.com",
-];
+        "https://google.com",
+        "https://rust-lang.org",
+        "https://github.com",
+        "https://google.com", // Repetida: Debería leer de caché
+        "https://stackoverflow.com",
+        "https://rust-lang.org", // Repetida: Debería leer de caché
+        "https://wikipedia.org",
+        "https://reddit.com",
+        "https://github.com", // Repetida: Debería leer de caché
+        "https://amazon.com",
+    ];
 
     {
         let mut tareas_u = tareas.lock().unwrap();
@@ -25,25 +31,39 @@ pub fn wscom(){
         }
     }
 
-
     for _ in 0..4 {
         let tareas_h = tareas.clone();
         let cache_h = cache.clone();
         let handle = thread::spawn(move || {
             loop {
-                if let Some(tarea) = tareas_h.lock().unwrap().pop_back() {
-                drop(tarea);
-                let content_web = thread::sleep(Duration::from_millis(500));
-                cache_h.lock().unwrap().insert("hola".to_string(), format!("content html {:?}", content_web));
+                let mut cola = tareas_h.lock().unwrap();
+                if let Some(tarea) = cola.pop_back() {
+                    drop(cola);
+
+                    //para tarea cacheada, es decir, si tengo dos veces github no descargalar dos veces
+                    {
+                        let cache_l = cache_h.lock().unwrap();
+                        if cache_l.contains_key(&tarea) {
+                            println!("tarea cacheada {}", tarea);
+                            continue;
+                        }
+                    }
+
+                    let content_web = thread::sleep(Duration::from_millis(500));
+                    println!("a descargar la tarea {}", { &tarea });
+                    cache_h
+                        .lock()
+                        .unwrap()
+                        .insert(tarea, format!("content html {:?}", content_web));
+                } else {
+                    break;
+                }
             }
-        }
         });
-    handles.push(handle);
-    };
+        handles.push(handle);
+    }
 
-
-for h in handles {
-    h.join().unwrap();
-}
-
+    for h in handles {
+        h.join().unwrap();
+    }
 }
